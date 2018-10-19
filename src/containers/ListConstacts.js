1e39e77d86884183eb8c67ea443d413b44b0de64
MatthewDodi <matthew.dodi@gmail.com>
2018-10-19 15:28:52 -0700

    created basic UI design

---
+++ src/containers/ListConstacts.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const IMG = styled.div`
+  background-image: url(${props => props.url});
+`
+
+class ListContact extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5001/contacts', 
+    { headers: { 'Authorization': 'whatever-you-want' }})
+    .then(resp => resp.json())
+    .then(data => this.setState( {contacts: data.contacts} ));
+  }
+
+  render() {
+      
+    return (
+      <ul className="contact-list">
+        {this.state.contacts.map(contact => (
+          <li key={contact.id} className='contact-list-item'>
+            <IMG className="contact-avatar" url={contact.avatarURL}></IMG>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className="contact-remove"></button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default ListContact;
